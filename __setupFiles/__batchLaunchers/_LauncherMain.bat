b5dc09970b1b42e6d106aec282eb596abe736995
aloeal <allie.christensen@nist.gov>
2025-04-07 16:20:45 -0500

    Update _LauncherMain.bat

--- __setupFiles/__batchLaunchers/_LauncherMain.bat
+++ __setupFiles/__batchLaunchers/_LauncherMain.bat
@@ -1,11 +1,11 @@
 @echo off
-:: -- NIST ozOptics Software --
+:: -- NIST DCS Combs Controller Software --
 
-:: This bat file launches ozOptics polarization controller software
+:: This bat file launches DCS Combs Controller software
 :: - @ECHO lines do not print to console, only use rem within loops and :: outside loops for comments
 
 :: NEW Setup: 
-:: change PATH_ on line 15 in "" to path where >sysCode\ozOptics\ is located on PC getting eBUS
+:: change PATH_ on line 15 in "" to path where >sysCode\DCSCombControl\ is located on PC getting eBUS
 :: change PATH_PYTHON on line 20 in "" to path where python executable (python.exe) is located on PC 
 :: ________________________________________________________________________________________________________________________________________
 
@@ -17,12 +17,12 @@ setlocal enabledelayedexpansion
 set allie=1
 
 :: expedite path setting for allie on sslap
-if !allie! == 1 ( set "PATH_=C:\Users\anc32\GitItUp\ozOptics\__setup" & set "PATH_PYTHON=C:\Python_3913" & goto :done )
+if !allie! == 1 ( set "PATH_=C:\Users\anc32\GitItUp\DCSCombControl\__dcsCombsetup" & set "PATH_PYTHON=C:\Python_3913" & goto :done )
 
 :: path to repo
-set "PATH_=C:\OTTRepos\ozOptics\"
+set "PATH_=C:\Users\fcomb\OTTRepos\DCSCombControl\__dcsCombsetup"
 :: alt path to repo
-rem set "PATH_=C:\Users\fcomb\OTTRepos\ozOptics\"
+rem set "PATH_=C:\Users\fcomb\OTTRepos\DCSCombControl\"
 
 :: path to installed python on pc
 set "PATH_PYTHON=C:\WPy64-3940\python-3.9.4.amd64\python.exe"
@@ -34,7 +34,7 @@ rem set "PATH_PYTHON=C:\WinPy3.9.4\WPy64-3940\python-3.9.4.amd64\python.exe"
 :: ________________________________________________________________________________________________________________________________________
 :: bool if user wants option to debug terminal startup 
 set debug=1
-set "venvName=__ozVenv"
+set "venvName=__dcsVenv"
 :: ________________________________________________________________________________________________________________________________________
 echo _______________________________________________________________________
 echo ----------------- DCS Combs Controller Software -----------------------
@@ -75,7 +75,7 @@ set "WORK_DIR=%PATH_%sysCode\"
 cd %PATH_%
 
 :: ________________________________________________________________________________________________________________________________________
-echo                         ozOptics venv: !SETUP!
+echo                         dcsCombs venv: !SETUP!
 echo ________________________________________________________________________
 echo.
 :: ________________________________________________________________________________________________________________________________________
@@ -85,7 +85,7 @@ echo.
 if %debug% ==1 ( echo expect venv dir & echo !SETUP!activate.bat & pause )
 
 :: Ensure virtual environment exists if not create it with local python installed 
-if not exist !SETUP!activate.bat ( echo ERROR: Virtual environment not found^! Ensure venvSetup.bat build venv in FSO. & pause && exit /b )
+if not exist !SETUP!activate.bat ( echo ERROR: Virtual environment not found^! Ensure setup.bat build venv in dcs. & pause && exit /b )
 
 :: Activate the virtual environment
 call !SETUP!activate.bat
