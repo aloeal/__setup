27780a713dbdd80af7263c1a629f81a0fc5115a2
aloeal <allie.christensen@nist.gov>
2025-04-07 14:52:10 -0500

    Final updates

--- __setupFiles/__batchLaunchers/.updateSubmod.bat
+++ __setupFiles/__batchLaunchers/.updateSubmod.bat
@@ -1,18 +1,12 @@
-rem @echo off
-rem :: Batch file to update submodules in repot with most RECENT commit in submod repo
-rem :: ________________________________________________________________________________________________________________________________________
-
-rem :: Ensure script runs as administrator
-rem net session >nul 2>&1
-rem if %errorlevel% neq 0 (
-rem     echo Elevating privileges...
-rem     powershell -Command "Start-Process cmd.exe -Verb RunAs -WorkingDirectory %~dp0 " && exit /b ) 
-:: ________________________________________________________________________________________________________________________________________
-
 @echo off
+
+:: Batch file to update submodules in repot with most RECENT commit in submod repo
+
+:: ________________________________________________________________________________________________________________________________________
 
 :: so pc wont remember variables to enviroment
 setlocal enabledelayedexpansion
+
 :: ________________________________________________________________________________________________________________________________________
 
 :: move to top level parent dir 
@@ -26,30 +20,24 @@ echo Updated submodule^!
 goto :close
 
 
+:: ________________________________________________________________________________________________________________________________________
+
 :confirm
-
-
-
 
 :: as user which python dir
 echo Overwrite local? & set /p over="y or n: "
 
-
 :: when changing path option enabled
-if /i !over! == y ( 
-    git submodule update --remote  --force || ( echo -n | set /p=ERROR %errorlevel%: & IF %errorlevel% == 128 ( echo NOT IN PARENT REPO^! ) else ( echo Try again. ) & goto :close )
-)
+if /i !over! == y ( git submodule update --remote  --force || ( echo -n | set /p=ERROR %errorlevel%: & IF %errorlevel% == 128 ( echo NOT IN PARENT REPO^! ) else ( echo Try again. ) & goto :close ) )
 
 if /i !over! neq y (
     echo ummm....nah
     rem grab other errors
     if /i !over! neq n ( echo. & echo ERROR: Please input "y" or "n" & echo Please try again^. & goto :confirm )
-    if /i !over! == n ( echo -n | set /p="... exiting!" && goto :close ) 
-)
+    if /i !over! == n ( echo -n | set /p="... exiting!" && goto :close ) )
 
 
-
-:: _____________
+:: ________________________________________________________________________________________________________________________________________
 
 :close
 
