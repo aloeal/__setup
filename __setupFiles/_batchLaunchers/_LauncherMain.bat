0136f75d739fe99fbd093c9751a6a4a338f6baf5
aloeal <allie.christensen@nist.gov>
2025-09-15 14:57:55 -0500

    included funks for startup and env_vars

--- __setupFiles/_batchLaunchers/_LauncherMain.bat
+++ __setupFiles/_batchLaunchers/_LauncherMain.bat
@@ -1,120 +1,45 @@
-@echo off
 
-:: -- NIST OchoBays Diode Controller Software --  
-
-
-:: This bat file launches eBUS software for camera/galvo control
-:: - @ECHO lines do not print to console, only use rem within loops and :: outside loops for comments
-
-:: NEW Setup: 
-:: change PATH_ on line 15 in "" to path where >\FSOTerminal-main\camera_control\cameraprocess is located on PC getting eBUS
-:: change PATH_PYTHON on line 20 in "" to path where python executable (python.exe) is located on PC 
-:: ________________________________________________________________________________________________________________________________________
-
-:: so pc wont remember variables to enviroment DO NOT REMOVE below line
-setlocal enabledelayedexpansion
-
-
+@echo off & setlocal enabledelayedexpansion 
+%= above line = MUST have pc not remember variables to enviroment DO NOT REMOVE =% 
 
 
 :: ________________________________________________________________________________________________________________________________________
+        %= output script information to terminal for user  =% 
 
 
-:: potential repository paths
-
-echo Searching for "...\!repo!*" .... 
+if %loser%==True ( goto :initilization ) else ( echo testing intro... )
 
 
-set "repoNames=%repo% %repo%-local %repo%-main"
-set flagA=False 
- 
-:: Loop through each path in repoPATHs
-for %%P in (%repoPATHs%) do (
-    set "currentPath=%%~P\"
-    for %%Q in (!repoNames!) do (
-        set "currentRepo=%%~Q\" 
-
- 
-        if exist "!currentPath!!currentRepo!" (
-
-            set flagA=True
-            set "PATH_=!currentPath!!currentRepo!"
-            echo -n |set /p="echo !currentPath!!currentRepo!!dir!\"
-
-            goto :repoFound 
-
-
-        ) else (
-            rem echo -n |set /p="...!currentPath!!currentRepo!...x"  
-        )
-    )
-)
-
-
-
-if %flagA% == False ( echo Initializing ERROR: Repo not found. & pause & exit /b )
-
-:repoFound
-echo. & echo    -^> REPO found : !PATH_! & echo. 
- 
-:: ________________________________________________________________________________________________________________________________________
-
-call :load_var
-
+echo -- aloeBrooks automated system software launcher file -- 
+echo Last mod: Sep 15 2025
+echo Created by: Allie Christensen Brooks
 
 :: ________________________________________________________________________________________________________________________________________
+        %= ensure prop dir and load files needed to setup software in __setupFiles =% 
+
+:initilization
+
+echo ****************** & echo starting up... & echo ******************
+
+cd /d "%~dp0\..\.." & call :displayCwd
+
+if  %loser%==True ( echo loserville ) else ( echo its giving our great trimphed czar & pause )
+
+rem *********************************************************************************************
+        %= import funks.bat and requirements.txt and env_vars.txt =% 
 
 
-set flagB=False
- 
-:: potential python paths
-echo Searching for %pyType% %pyType% ...\python.exe ...
+rem load file with batch functions to initalize terminal operation and repo location
+call :load_funks | echo ERROR loading funkies oh no!
 
-:: Loop through each path in repoPATHs
-for %%P in (%pyPATHs%) do (
-    set "currentPath=%%~P\"
+rem run funks.bat to set repo location PATH_ and respectively python or winpython path 
+call :startFunky
 
-    if exist "!currentPath!python.exe" (
-        set flagB=True 
-
-        set "PATH_PYTHON=!currentPath!"
-        set "PYTHON_EXE=!currentPath!python.exe"
-
-        goto :pythonFound
-
-    ) else (
-         rem echo -n |set /p="... !currentPath!python.exe...x"
-
-    )
-)
-
-if %flagB% == False ( echo Python not found. Install required -^> & goto :decompressExe )
-
-:pythonFound
-echo. 
-echo    -^> PYTHON found : !PATH_PYTHON! 
+if  %loser%==True ( goto :startBat ) else ( echo "Your a winner chckn dinner, env comming in hot" & pause )
 
 :: ________________________________________________________________________________________________________________________________________
-
-set debug=0
-
-:: ________________________________________________________________________________________________________________________________________
-
-echo ------------- NIST OchoBays Diode Controller Software ------------------  
-echo NIST: 675.02                                     Last mod: july 7 2025
-echo                                                    Allie Christensen                                                          
-echo ------------------------------------------------------------------------
-echo.
-echo.
-
-
-:: ________________________________________________________________________________________________________________________________________
-                %= setup paths needed for installations =% 
-:startFSO
-
-:: dynamic path DO NOT CHANGE below
-set "SETUP=%PATH_%%venvName%\Scripts\activate.bat" 
-set "WORK_DIR=%PATH_%sysCode\camera_control\cameraprocess\"
+                %= activate environment and move into working dir =% 
+:actVenv
 
 cd %PATH_%
 
@@ -123,21 +48,13 @@ echo                         !repo! venv:
 echo    ^> !SETUP!
 echo ________________________________________________________________________
 echo.
-:: ________________________________________________________________________________________________________________________________________
-                %= activate environment and move into working dir =% 
-:actVenv
 
-echo Calling... !SETUP!
 
-:: Ensure virtual environment exists if not create it with local python installed 
+rem Ensure virtual environment exists if not create it with local python installed 
 if not exist %SETUP% ( echo ERROR: Virtual environment not found^! Ensure venvSetup.bat build venv in FSO. & pause && exit /b )
 
 :: Activate the virtual environment
 call %SETUP% || ( echo ERROR: Virtual environment activation FAIL^^! Attempt manual & cmd /k )
-
-:: ...Change working directory -> where python will work from
-cd !WORK_DIR!
-
 
 :: ________________________________________________________________________________________________________________________________________
 
@@ -146,24 +63,24 @@ cd !WORK_DIR!
 :: user wants to debug -> enable cmd /k 
 if %debug% neq 0 ( 
     echo debuggin^^! 
-    python %WORK_DIR%%file% || ( echo ERROR DEBUG: oh lets get it & cmd /k )
+    %pyPATHs% %repoPATHs%%file% || ( echo ERROR DEBUG: oh lets get it & cmd /k )
 )
 if %debug% == 0 ( 
     echo +ultra-mode...
-    python %WORK_DIR%%file% || ( echo oh hell nah...try again & pause & exit )
+    %pyPATHs% %repoPATHs%%file% || ( echo oh hell nah...try again & pause & goto :close )
 )
 
 
 
 :: ________________________________________________________________________________________________________________________________________
 
-endlocal
-deactivate
-
-
 
 
 :load_var
     call %PATH_%\__setup\__setupFiles\__files\env_vars.txt
-    echo "      == RELOADED env_var.txt =="
-    exit /b
+    echo "      == RELOADED env_var.txt ==" & exit /b
+
+:close
+    endlocal
+    deactivate
+    exit 
