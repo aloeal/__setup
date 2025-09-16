699dff4ca872c5d7c3b88381df6b8ee9d0b773f5
aloeal <allie.christensen@nist.gov>
2025-09-16 10:54:58 -0400

    should be debugged batch

--- __setupFiles/_batchLaunchers/.relativeUse/_remove__Setup.bat
+++ __setupFiles/_batchLaunchers/.relativeUse/_remove__Setup.bat
@@ -1,37 +1,44 @@
-:: ________________________________________________________________________________________________________________________________________
-@echo off
 
-rem  initial admin terminal start and dont remember variables -> messes with pc env
+@echo off & setlocal enabledelayedexpansion 
+%= above line = MUST have pc not remember variables to enviroment DO NOT REMOVE =% 
 
-
-:: Ensure script runs as administrator
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Elevating privileges...
-    powershell -Command "Start-Process cmd -ArgumentList '/c \"\"%~f0\"\"' -Verb RunAs -WindowStyle Normal" && exit /b
-)
-
-@echo off
-
-:: so pc wont remember variables to enviroment
-setlocal enabledelayedexpansion
 
 :: ________________________________________________________________________________________________________________________________________
-
-:: path to dirs (do not change)
-set "repo=FSOTerminal"
-
-:: potential repository paths
-set repoPATHs="C:\OTTRepos" "C:\Users\fcomb\OTTRepos" "C:\Users\anc32\GitItUp" "C:\Users\fcomb\GitHub" "C:\temp"
+        %= output script information to terminal for user  =% 
 
 
-set "repoNames=%repo% %repo%-local %repo%-main"
-set "branch=cooked_2025"
+if %loser%==True ( goto :initilization ) else ( echo testing intro... )
 
 
-set subs=__setup galvo_control
+echo -- aloeBrooks automated submodule _flex debugger file -- 
+echo Last mod: Sep 15 2025
+echo Created by: Allie Christensen Brooks
 
-set "pause=True"
+:: ________________________________________________________________________________________________________________________________________
+:: ________________________________________________________________________________________________________________________________________
+        %= ensure prop dir and load files needed to setup software in __setupFiles =% 
+
+:initilization
+
+echo ****************** & echo starting up... & echo ******************
+
+cd /d "%~dp0\..\.." & call :displayCwd
+
+if  %loser%==True ( echo loserville ) else ( echo its giving our great trimphed czar & pause )
+
+rem *********************************************************************************************
+        %= import funks.bat and requirements.txt and env_vars.txt =% 
+
+
+rem load file with batch functions to initalize terminal operation and repo location
+call :load_funks | echo ERROR loading funkies oh no!
+
+rem run funks.bat to set repo location PATH_ and respectively python or winpython path 
+call :startFunky
+
+if  %loser%==True ( goto :startBat ) else ( echo "Your a winner chckn dinner, env comming in hot" & pause )
+:: ________________________________________________________________________________________________________________________________________
+
 
 set closetime=15 
 rem units of seconds
@@ -64,17 +71,18 @@ for %%P in (%repoPATHs%) do (
 
             set flagA=True
             set "PATH_=!currentPath!!currentRepo!"
-            if %pause% == True ( echo -n |set /p="!currentPath!!currentRepo!" )
+            if %debug% == True ( echo -n |set /p="!currentPath!!currentRepo!" )
 
             goto :repoFound 
 
 
-        ) & rem else (  rem if %pause% == True ( rem ( echo -n |set /p="...!currentPath!!currentRepo!...x" )  & echo. ) )
+        ) & rem else (  rem if %debug% == True ( rem ( echo -n |set /p="...!currentPath!!currentRepo!...x" )  & echo. ) )
     )
 )
 
 
-if %flagA% == False ( echo Initializing ERROR: Repo not found. & pause & exit /b )
+if %flagA% == False ( echo Initializing ERROR: Repo not found. & if !debug! ==1 ( echo debug me & cmd /k ) else ( pause & echo no debug & exit /b )) 
+
 
 :: ________________________________________________________________________________________________________________________________________
 
@@ -92,14 +100,15 @@ echo.
 cd %PATH_% >nul
 git switch %branch% >nul
 git pull >nul
-git stash clear >nul || ( echo -- did not CLEAR stash ^!  -- )
+
+call :clearStash
 
 :: ________________________________________________________________________________________________________________________________________
 
 
-if not EXIST .gitattributes ( set "skipFastAtt=True" ) 
-if not EXIST .gitmodules ( set "skipFastMod=True" ) 
-git config --get-regexp submodule 2>nul || ( set "skipFastConfig=True" ) 
+if not EXIST .gitattributes ( set "skipFastAtt=True" & echo .GITATT ^| done ) 
+if not EXIST .gitmodules ( set "skipFastMod=True" & echo .GITMOD ^| done ) 
+( git config --get-regexp submodule 2>nul && git submodule status 2>nul ) || ( set "skipFastConfig=True" & echo .config ^| done ) 
 
 rem to skip config 
 if %skipFastConfig% == True ( 
@@ -187,7 +196,7 @@ for %%I in (%subs%) do (
 
 if %flag% GTR 0 (  rmdir /S /Q .git\modules & set flag=0 || ( echo      -^> Unable to delete module within ^.git )  )
 
-if %pause% == True ( pause )
+if %debug% == True ( pause )
 
 ( git config -f .git/config --unset submodule.active 2>nul && echo                                         -^> ^| Submodules INactive ) || echo ERROR Unsetting did not work OR no submodule to unset
 
@@ -203,47 +212,65 @@ rem git add -A 2>nul || echo      -^> No staged changes to remove
 git clean -fdx 2>nul || echo nothing to clean 
 if %printouts%==True ( echo. & echo     ----------------------- & echo         -- CLEANED -- & echo        ----------------------- ) 
 
-if %pause% == True (  pause )
 
 :: ________________________________________________________________________________________________________________________________________
 
 :commit
-git stash clear >nul || ( echo -- did not CLEAR stash ^!  -- )
 
 echo ------------------------------------------------------------
 
-echo Dry run of commit and then commit stage...
+call :clearStash
+
+if %debug% == True (  pause )
+
+echo ------------------------------------------------------------
 
 
 if %dryRun% == True ( echo DRY-RUN mode - no real commit OR push going to close... & pause & goto :close )  
 
+if %skipFastAtt% == True ( echo cestFin 3 & goto :skipAtt )
+echo ------------------------------------------------------------
+
+
 rem add all config and git changes to stage
 git add .gitattributes 2>nul
+
 git commit --dry-run -m "Updated .gitattributes -> remove lfs " || ( goto :skipAtt ) 
-if %pause% == True ( echo ...ok to commit ^.gitattributes^? & pause ) else ( timeout /nobreak /t 1 2>nul ) 
+if %debug% == True ( echo ...ok to commit ^.gitattributes^? & pause ) else ( timeout /nobreak /t 1 2>nul ) 
 
 git commit -m "Updated .gitattributes -> remove lfs "
 
+echo ------------------------------------------------------------
 
 :skipAtt
+
+if %skipFastMod% == True (echo cestFin 2 &  goto :skipMod )
 git add .gitmodules 2>nul 
+
 git commit --dry-run -m "Updated .gitmodules -> remove submodule " || ( goto :skipMod ) 
-if %pause% == True ( echo ...ok to commit ^.gitmodules^? & pause ) else ( timeout /nobreak /t 1 2>nul ) 
+if %debug% == True ( echo ...ok to commit ^.gitmodules^? & pause ) else ( timeout /nobreak /t 1 2>nul ) 
 
 git commit -m "Updated .gitmodules -> remove submodule "
 
+echo ------------------------------------------------------------
 
 :skipMod
 
 rem commit the rest -> commits removal of submodules
 for %%I in (%subs%) do ( 
-    git add %%I 2>nul 
-    git commit --dry-run -m "Removing %%I submodule traces from branch"  || ( goto :cestFin ) 
+    git diff --name-only --cached -- %%I || ( set "staged=False" & echo set stage false & git add %%I 2>nul )
 
-    if %pause% == True ( echo ...ok to commit rest^? & pause ) else ( timeout /nobreak /t 1 2>nul ) 
+
+    git commit --dry-run -am "Removing %%I submodule traces from branch"  || ( goto :cestFin ) 
+
+    if %debug% == True ( echo ...ok to commit rest^? & pause ) else ( timeout /nobreak /t 1 2>nul ) 
 
     git commit -m "Removing %%I submodule traces from branch" 
     ) 
+
+if %skipFastConfig% ==True (echo cestFin 1 )
+
+
 
 
 echo ------------------------------------------------------------
@@ -266,52 +293,82 @@ goto :close
 
 :close
 
-rem closing procedures
+    rem closing procedures
 
-echo --------------^> CLOSING in %closetime% sec^! 
+    echo --------------^> CLOSING in %closetime% sec^! 
 
 
 
-for /L %%i in (%closetime%, -1, 1) do (
-    rem <nul echo -n | set /p=%%i... 
-    <nul set /p= CLOSING in %%i sec...
- 
-    timeout /nobreak /t 1 >nul
-)
+    for /L %%i in (%closetime%, -1, 1) do (
+        rem <nul echo -n | set /p=%%i... 
+        <nul set /p= CLOSING in %%i sec...
+     
+        timeout /nobreak /t 1 >nul
+    )
 
-echo.
-echo -n | set /p=BYE^!
+    echo.
+    echo -n | set /p=BYE^!
 
-endlocal 
-exit
+    endlocal 
+    exit
 
 :: ________________________________________________________________________________________________________________________________________
 
 
 :verify
 
-rem check repo status and ensure clean 
-echo. 
-echo ___________________________ VERIFY REPO CLEAN ___________________________
-echo.
-echo            -^>  ORIGIN ^& LOCAL state NA if clean.
-echo. 
-echo -n | set /p="ORIGIN :"  & git config --get-regexp --show-origin submodule || echo          NA
-echo ------------------------------------------------------------
-echo -n | set /p="LOCAL :" & git config --get-regexp submodule || echo          NA
-echo.
-echo ____________________________________________________________________________
-echo. & exit /b 
+    rem check repo status and ensure clean 
+    echo. 
+    echo ___________________________ VERIFY REPO CLEAN ___________________________
+    echo.
+    echo            -^>  ORIGIN ^& LOCAL state NA if clean.
+    echo. 
+    echo -n | set /p="ORIGIN :"  & git config --get-regexp --show-origin submodule || echo          NA
+    echo ------------------------------------------------------------
+    echo -n | set /p="LOCAL :" & git config --get-regexp submodule || echo          NA
+    echo.
+    echo ____________________________________________________________________________
+    echo. & exit /b 
 
 
 :intro
-rem remove all submodule refs 
-echo            ----------------------- current config ------------------------
-echo. 
-echo -n | set /p=" .CONFIG"
-git config --get-regexp submodule 
-echo -n | set /p=" :STATUS"
-git submodule status
-echo. 
-echo            -------------------- adjusting config ... ---------------------
-exit /b
+    rem remove all submodule refs 
+    echo            ----------------------- current config ------------------------
+    echo. 
+    echo -n | set /p=" .CONFIG"
+    git config --get-regexp submodule 
+    echo -n | set /p=" :STATUS"
+    git submodule status
+    echo. 
+    echo            -------------------- adjusting config ... ---------------------
+    exit /b
+
+
+:load_var
+    cd . 
+    echo here
+    cd 
+    call .\__setupFiles\__files\env_vars.bat || (
+        echo [ERROR] Failed to load env_vars.bat
+        exit /b 
+    )
+    echo [INFO] == Reloaded env_vars.txt ==
+    exit /b
+
+
+:load_funks
+    cd . 
+    echo here
+    cd 
+    call .\__setupFiles\_batch\.relativeUse\funks.bat || (
+        echo [ERROR] Failed to load funks.bat
+        exit /b 
+    )
+    echo [INFO] == Reloaded funks.bat ==
+    exit /b
+
+:clearStash
+    git stash clear >nul || ( echo -- did not CLEAR stash ^!  -- )
+    exit /b 
+
+:: ________________________________________________________________________________________________________________________________________
