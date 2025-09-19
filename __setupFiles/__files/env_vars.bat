0ef7d926eae54469bae2e59a304dd8d1065fde03
aloeal <allie.christensen@nist.gov>
2025-09-19 10:24:46 -0400

    update env var file to generalize
    Updated env vars to accounts for alt install

--- __setupFiles/__files/env_vars.bat
+++ __setupFiles/__files/env_vars.bat
@@ -1,20 +1,36 @@
 :: env_vars.txt
-set "repo=FSOTerminal"
-set "venvName=__fsoVenv"
-set "dir=__fastBadJubbies"
-set "file=camera_processor_V1_2.py"
-set repoPATHs="C:\OTTRepos" "C:\Users\fcomb\OTTRepos" "C:\Users\anc32\GitItUp" "C:\Users\fcomb\GitHub" "C:\GitHub" "C:\temp"
-set "repoNames=%repo% %repo%-local %repo%-main"
-set "branch=cooked_2025"
 
 
-set subs=__setup galvo_control
+set "repo=moomoo"
+
+set "branch=itsGivingAlpha"
+set "file=moosAintEddy.py"
 
 
-set "pyType=Python"
+set "pyType=python"
 set "pyVersion=3.11.9"
 
-set pyPATHs="C:\Program Files\WPy64-3940" "C:\WinPy3.9.4\WPy64-3940\python-3.9.4.amd64" "C:\WPy64-3940\python-3.9.4.amd64"
+#-------------------------------------------------
+rem do hot change below unless avid user
 
+set repoPATHs="C:\OTTRepos" "C:\Users\fcomb\OTTRepos" "C:\Users\anc32\GitItUp" "C:\Users\fcomb\GitHub" "C:\GitHub"  "C:\temp"
 
-set bonus="voidtools.Everything" "Microsoft.VCRedist.2013.x86"
+set "repoNames=%repo% %repo%-local %repo%-main"
+
+set "venvName=__alphaVenv"
+rem directory for the _flex submodules to be plopped
+set "dir=__daddy"
+
+set "subs=_flex"
+
+#-------------------------------------------------
+
+if /I %pyType%=="win" ( set pyPATHs="C:\Program Files\WPy*" "C:\Program Files\WinPy*" "C:\WinPy*" "C:\" 
+) 
+if /I %pyType%=="win" ( set pyPATHs="C:\Program Files\Py*" "C:\Program Files\py*" "C:\py*" "C:\" 
+) 
+
+else ( set pyPATHs="C:\Program Files\" "C:\users\" "C:\"  )
+
+rem extra applications needing install
+rem set bonus="voidtools.Everything" "Microsoft.VCRedist.2013.x86"
