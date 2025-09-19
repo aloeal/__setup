c3c36b5118551412f1505b8f40fc412738a0e52a
aloeal <allie.christensen@nist.gov>
2025-09-19 14:28:36 -0400

    redo of the env_var file to be user-friendly

--- __setupFiles/_batchLaunchers/.relativeUse/env_vars.bat
+++ __setupFiles/_batchLaunchers/.relativeUse/env_vars.bat
@@ -23,17 +23,41 @@ set pyPATHs="C:\Program Files\WPy64-3940" "C:\WinPy3.9.4\WPy64-3940\python-3.9.4
 
 
 
+set skipPython=0
+
 rem _________________________________________________________________________
 rem DO NOT CHANGE below
 
-set "repoNames=%repo% %repo%-local %repo%-main"
+set +ultra=True & rem fastest setup method
+
 set debug=0
+
+
+if %skipPython%==0 ( set skipVenv=0 ) else ( set skipVenv=1 )
+if %+ultra%==True ( set ask=0 ) else ( set ask=1)
+
+rem to install git bash into repo 
+set skipGit=0
+
+rem below skips need to uncompress bonus and/or exe files 
+set skipExe=1
+
+rem whether to skip .exe or uncompressed files in __files 
+set skipBonus=1
+
+
+
+set closetime=30
+set waittime=3
 set "loser=False"
 set exeName=None
 
-if /I %pyType%=="python" ( set exeName="python.exe" ) 
-if %pyType%=="winpython" (set exeName="WinPython Command Prompt.exe" ) else ( set "exename=!pyType!" )
 
+set "repoNames=%repo% %repo%-local %repo%-main"
+
+
+if /I %pyType%=="python" ( set exeName="python.exe" ) 
+if /I %pyType%=="winpython" (set exeName="WinPython Command Prompt.exe" ) else ( echo !pyType! & pause & cmd /k & set "exename=!pyType!.exe" )
 echo set exename
 echo %exeName%
 
@@ -43,6 +67,8 @@ rem unused (for later) dynamic path
 rem set "SETUP=%PATH_%%venvName%\Scripts\activate.bat" 
 rem set "WORK_DIR=%PATH_%sysCode\camera_control\cameraprocess\"
 rem set bonus="voidtools.Everything" "Microsoft.VCRedist.2013.x86"
+
+rem set bonus=3
 rem set "venvName=__fsoVenv"
 rem set "initBranch=freeSpaceOptics"
 rem set "dir=_init"
