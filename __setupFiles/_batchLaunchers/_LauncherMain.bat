29befd542c719f1db048bbbd98f4bf9284af24c1
aloeal <allie.christensen@nist.gov>
2025-09-22 13:48:17 -0400

    adjusted commenting and compiling

--- __setupFiles/_batchLaunchers/_LauncherMain.bat
+++ __setupFiles/_batchLaunchers/_LauncherMain.bat
@@ -2,36 +2,30 @@
 rem *********************************************************************************************
 rem *********************************************************************************************
 rem *********************************************************************************************
-
 @echo off & setlocal enabledelayedexpansion 
-%= above line = MUST have pc not remember variables to enviroment DO NOT REMOVE =% 
-
-:: ________________________________________________________________________________________________________________________________________
+rem above line = MUST have pc not remember variables to enviroment DO NOT REMOVE
+rem *********************************************************************************************
         %= output script information to terminal for user  =% 
 
 
-set "lastMod=Sep 22 2025"
-set "title=automated system software "
 
-:: ________________________________________________________________________________________________________________________________________
-:: ________________________________________________________________________________________________________________________________________
-        %= ensure prop dir and load files needed to setup software in __setupFiles =% 
+set "lastMod=Sep 22 2025"
+set "title=automated system software launcher "
+
+
+rem *********************************************************************************************
+        %= import funks.bat and requirements.txt and env_vars.txt =% 
 
 :initilization
 
 
 if %loser%==True ( echo  its giving our great trimphed czar & goto :start ) else ( echo testing intro... )
 
-rem *********************************************************************************************
-        %= import funks.bat and requirements.txt and env_vars.txt =% 
-
-
 rem load file with batch functions to initalize terminal operation and repo location
 call :load_funks | echo ERROR loading funkies oh no!
 
 rem run funks.bat to set repo location PATH_ and respectively python or winpython path 
-call :startFunky | echo ERROR starting the funk 
-
+call :_init %file% | echo ERROR starting the funk 
 
 call :helloSoftware %title% %lastMod%
 
@@ -42,22 +36,9 @@ rem ****************************************************************************
 
 
 :start
-echo started...
+echo started... and ready? & pause 
 
-
-:: ________________________________________________________________________________________________________________________________________
-
-:run
-
-:: user wants to debug -> enable cmd /k 
-if %debug% neq 0 ( 
-    echo debuggin^^! 
-    %pyPATHs% %repoPATHs%%file% || ( echo ERROR DEBUG: oh lets get it & cmd /k )
-)
-if %debug% == 0 ( 
-    echo +ultra-mode...
-    %pyPATHs% %repoPATHs%%file% || ( echo oh hell nah...try again & pause & goto :exit )
-)
+call :runFile
 
 
 :: ________________________________________________________________________________________________________________________________________
