4635cc204f2422a24f34f56066978925417d0bb6
aloeal <allie.christensen@nist.gov>
2025-09-25 12:26:56 -0400

    debugged batch funks scripts
    labels adjusted to end according to routine used in call ( exit /b for subroutine related labels) and further debugging to use as an automation feature

--- __setupFiles/_batchLaunchers/relativeUse/funks.bat
+++ __setupFiles/_batchLaunchers/relativeUse/funks.bat
@@ -1,103 +1,105 @@
 
 @echo off & setlocal enabledelayedexpansion
+:_init
+    echo hello
+    set "parent=%~1"
+    echo %~1
+    call :load_var
+
+    call :showCWD
+
+    call :helloScript
+    pause 
+    if "%parent%"=="%file%" ( echo alpha & goto :startAlpha) else ( echo funny & call :startFunky)
+    exit /b
+
 
 
 :startFunky
-    call :_init
+    echo start funk 
+    pause
 
     rem look for repository cloned ontol pc using generalized paths in env_var.bat
     call :findDir
-
-    call :switchGitPull
-
+    echo funky alpha
+    rem call :switchGitPull
+    echo funky beta
+    pause
     rem look for pyType installed on pc or install 
     call :find_py
-
+    echo funky charlie
+    pause
     rem set requested venv path as variable for parent batch
-    call :setVenv
-
-    rem determine if venv exists or needs creation
-    call :chkVenv
-
+    call :startAlpha
+    echo funky delta
+    pause
     rem belows essentially returns code back to file that called originally
     goto :eof
 
 :startAlpha
-    call :_init
+    echo start alpha & pause
 
     rem determine if venv exists or needs creation
+    call :setVenv
+    
     call :chkVenv
-
+    echo funky epsilon
+    pause
     call :actVenv
-    
+    echo funky freddy
+    pause
+    call :createVenv
     rem belows essentially returns code back to file that called originally
-    goto :eof
+    exit /b
 
 
-:_init
-    set "parent=%~1"
-
-    rem open an admin terminal for user to ensure permissions are not an issue with installations 
-    call :openAdmin
-    
-    cd /d "%~dp0\..\..\.."
-    call :helloScript
-
-    rem import variables from env_var.bat
-    call :load_var
-
-    rem belows essentially returns code back to file that called originally
-    if %parent% == "%file%" ( call :startAlpha & echo alpha ) else ( call :startFunky & echo funky )
     
 rem *********************************************************************************************
 rem *******   sys & user                             *******************************************
 rem *********************************************************************************************
 
+:showCWD
+    echo CWD:
+    cd 
+    exit /b
+   
+
+
 :openAdmin
 	rem Ensure script runs as administrator
 	
-	set "PATH_script=%~dp0"
+	set "PATH_script=%~dp0\..\.."
 
 	net session >nul 2>&1
 	if %errorlevel% neq 0 (
 	    echo Elevating privileges...
-	    powershell -Command "Start-Process cmd -ArgumentList '/k cd /d ""%PATH_script%""' -Verb RunAs" && goto :eof
+	    powershell -Command "Start-Process cmd -ArgumentList '/k cd /d ""%PATH_script%""' -Verb RunAs" 
 	)
-    cd /d "%~dp0\..\.." & call :displayCwd
-
-    echo ****************** & echo starting up... & echo ******************
-
-:: ________________________________________________________
-
-
-:helloScript
-    echo -- aloeBrooks %repo% %branch%  -- Last mod: Sep 22 2025
     goto :eof
 
-:helloSoftware
-    set "title=%~1"
-    set "lastMod=%~2"
-    echo -- %title% -- Last mod: %lastMod%
-    goto :eof
 
-:: ________________________________________________________
+
+
+
 
 :load_var
 
-    echo where env_vars.bat & cd 
-    call env_vars.bat || (
+    echo where env_vars.bat 
+    cd
+     
+    call "./__setupFiles/_batchLaunchers/relativeUse/env_vars.bat" || (
         echo [ERROR] Failed to load env_vars.bat
-        goto :eof
+        pause
     )
-    echo "      == RELOADED env_var.txt =="
-    goto :eof
+    echo "      == LOADED env_var =="
+    exit /b
 
 :: ________________________________________________________
 
 :displayCwd
 
     echo -n | set /p=moved to.. current working dir: & cd
-    goto :eof
+    exit /b
 
 :: ________________________________________________________
 
@@ -117,64 +119,78 @@ rem call :askQ "winpython or python" "w" "p"
     if /i "!ans!" == "%ansTrue%" ( goto :resultTrue ) else if /i "!ans!" == "%ansFalse%" ( goto :resultFalse ) else ( echo NEITHER & call :resultOther )
 
     :resultFalse
-    rem reroute user back to question if answer is invalid
-    echo result false
-    goto :eof
+        rem reroute user back to question if answer is invalid
+        echo result false
+        exit /b 
 
     :resultTrue
-    rem update user with choosen installs avaliable versions
-    echo result true
-    goto :eof
+        rem update user with choosen installs avaliable versions
+        echo result true
+        exit /b
 
     :resultOther
-    rem reroute user back to question if answer is invalid
-    echo ERROR: Invalid input. Please enter "w" or "p". Please try again^.
-    goto :eof
+        rem reroute user back to question if answer is invalid
+        echo ERROR: Invalid input. Please enter "w" or "p". Please try again^.
+        exit /b
 
 
 rem *********************************************************************************************
 rem *******   data or git                            *******************************************
 rem *********************************************************************************************
+rem Loop through each path in repoPATHs
 
 
 :findDir
 
-    set flag=False 
-     
-    rem Loop through each path in repoPATHs
+    set "flag=False" 
+    set "FOUND_PATH="
+    echo searching for dir...
+    echo from %repoPATHs%...
     for %%P in (%repoPATHs%) do (
+        echo jam
         set "currentPath=%%~P\"
-        for %%Q in (!repoNames!) do (
+        echo !currentPath!
+
+        for %%Q in (%repoNames%) do (
+
             set "currentRepo=%%~Q\" 
 
-     
+            echo jub
+            echo !currentRepo!
+
             if exist "!currentPath!!currentRepo!" (
 
-                set flag=True
-                set "PATH_=!currentPath!!currentRepo!"
-                echo -n |set /p="echo !currentPath!!currentRepo!\"
+                set "flag=True"
+                set "FOUND_PATH=!currentPath!!currentRepo!"
+                echo -n |set /p="echo !currentPath!!currentRepo!"
 
-                goto :repoFound ) else ( echo -n |set /p="...!currentPath!!currentRepo!...x"   )
+                goto :repoFound 
+            ) else ( 
+                echo -n |set /p="...!currentPath!!currentRepo!...x"   
+            )
         )
      )
 
 
 
-    if %flag% == False ( echo Initializing ERROR: Repo not found. & if !debug! ==1 ( echo debug me & cmd /k ) else ( pause & echo no debug & goto :eof )) 
+    if /I %flag% == False ( 
+        echo Initializing ERROR: Repo not found. 
+        if %debug% ==1 ( echo debug me & cmd /k ) else ( pause & echo no debug & exit /b )
+    ) 
 
     :repoFound
     echo. & echo    -^> REPO found : !PATH_! & echo. 
-    goto :eof
-
+    set "PATH_=!FOUND_PATH!"
+    exit /b
 :: ________________________________________________________
 
 :switchGitPull
     cd %PATH_% >nul || ( echo bad %PATH_% & pause )
-    git switch %branch% >nul || ( echo switch ERTROR & pause )
+    git switch %branch% >nul || ( echo ERROR: GIT switch, may already be on correct branch... & git status & pause )
     echo confirm pull? & pause
-    call :askQ "Confirm pull of %repo%/%branch%" "y" "n" || ( echo error ASKQ & goto :eof )
+    call :askQ "Confirm pull of %repo%/%branch%" "y" "n" || ( echo error ASKQ & exit /b )
     git pull >nul || ( echo bad pull & pause )
-    goto :eof 
+    exit /b 
 :: ________________________________________________________
 
 :tryPull
@@ -191,7 +207,7 @@ rem ****************************************************************************
     )
 
     cd ..
-    goto :eof
+    exit /b
 :: ________________________________________________________
 
 :cleanRepo
@@ -213,7 +229,7 @@ rem ****************************************************************************
     echo -n | set /p=" :STATUS"
     git submodule status
     echo. 
-    goto :eof
+    exit /b
 
 :: ________________________________________________________
 
@@ -232,45 +248,58 @@ rem ****************************************************************************
     echo.
     echo ____________________________________________________________________________
     echo. 
-    goto :eof
+    exit /b
 
 :: ________________________________________________________
 
 :clearStash
     git stash clear >nul || ( echo -- did not CLEAR stash ^!  -- )
-    goto :eof
+    exit /b
 
 rem *********************************************************************************************
 rem *******   winpy or py                            *******************************************
 rem *********************************************************************************************
 
 :find_py
+    echo finding py...
+     
+    set "flag=False"
+    set "FOUND_PATH="
+    set "PYTHON_EXE="
+    
+    echo %pyPATHs%
+    echo %exeName%
+    pause 
 
-    set flag=False
-	rem Loop through each path in repoPATHs
 	for %%P in (%pyPATHs%) do (
-	    set "currentPath=%%~P\"
+        echo !currentPath!%exeName%
 
-	    if exist "!currentPath!%exeName%" (
-	        set flag=True 
+	    set "currentPath=%%~P"
+        pause 
+       
+   	    if exist "!currentPath!%exeName%" (
+            echo found 
+	        set "flag=True"
 
-	        set "PATH_PYTHON=!currentPath!"
+	        set "FOUND_PATH=!currentPath!"
 	        set "PYTHON_EXE=!currentPath!%exeName%"
 
 	        goto :found
 
-	    ) else (
-	         rem echo -n |set /p="... !currentPath!%exeName%...x"
-
-	    )
+        ) else ( pause & echo -n |set /p="... !currentPath!%exeName%...x" )  
 	)
 
-	if %flag% == False ( echo !pyType! not found. Install required... & where python & where WinPython & pause && exit /b )
+	if /I %flag% == False ( 
+        echo %pyType% not found. Install required... 
+        pause 
+        exit /b 
+    )
 
 	:found
 	echo. 
+    set "PATH_PYTHON=!FOUND_PATH!"
 	echo    -^> %pyType% found : !PATH_PYTHON! 
-	goto :eof
+	exit /b
 
 
 :: ________________________________________________________
@@ -279,7 +308,9 @@ rem ****************************************************************************
 
     set "venvPath_=%PATH_%%venvName%\" >nul || ( echo -- ERROR setting venv?  -- & pause & cmd /k)
     echo venv Path set -^> %venvPath_%
-    goto :eof
+    set "PATH_PYTHON=!FOUND_PATH!"
+
+    exit /b
 :: ________________________________________________________________________________________________________________________________________
 
 
@@ -287,7 +318,15 @@ rem ****************************************************************************
 
     rem Ensure virtual environment exists if not det sys installs
     if exist %SETUP% ( echo VENV FOUND ---- & set "flagVenv=True" ) else ( echo NONE^. Freezing cw setup... & pause && exit /b ) 
-    goto :eof
+    exit /b
+:: ________________________________________________________________________________________________________________________________________
+
+:createVenv
+    rem new venv creation
+    "%PYTHON_EXE%" -m venv %venvPath_% --prompt %venvName% 2>&1 || ( echo ERRORa !errorlevel!: Attempted to make env. && if %debug% == 1 ( echo Enabling cmd... && echo Ready! & cmd /k ) else ( echo oopsie & call :close ) )
+    rem if %debug% == 0 ( echo NOT in DEBUG MODE... & echo -n | ping -n 10 127.0.0.1 >nul && echo moving on! )
+    echo -n | set /p="Fresh venv created! "
+    exit /b
 
 :: ________________________________________________________________________________________________________________________________________
                 %= activate environment and move into working dir =% 
@@ -295,7 +334,7 @@ rem ****************************************************************************
 
     rem Activate the virtual environment
     call %SETUP% || ( echo ERROR: Virtual environment activation FAIL^^! Attempt manual & cmd /k )
-
+    exit /b
 :: ________________________________________________________________________________________________________________________________________
 
 :runFile
@@ -307,12 +346,31 @@ rem ****************************************************************************
     )
     if %debug% == 0 ( 
         echo +ultra-mode...
-        %pyPATHs% %repoPATHs%%file% || ( echo oh hell nah...try again & pause & goto :eof )
+        %pyPATHs% %repoPATHs%%file% || ( echo oh hell nah...try again & pause & exit /b )
     )
+
+
+
 
 rem *********************************************************************************************
 rem *******   exit procedures                            ***************************************
 rem *********************************************************************************************
+
+
+:: ________________________________________________________
+
+
+:helloScript
+    echo -- aloeBrooks %repo% %branch%  -- Last mod: Sep 22 2025
+    exit /b
+
+:helloSoftware
+    set "title=%~1"
+    set "lastMod=%~2"
+    echo -- %title% -- Last mod: %lastMod%
+    exit /b
+
+:: ________________________________________________________
 
 :close
 
